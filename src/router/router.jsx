8efb3168c19cf2b52ebb1172a69153c32e2732c8
aburayhan-bpi <aburayhan.bpi@gmail.com>
2024-12-22 14:45:20 +0600

    project basic setup done

---
+++ src/router/router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h2 className="font-bold text-red-600">Hi project</h2>,
+  },
+]);
+
+export default router;
